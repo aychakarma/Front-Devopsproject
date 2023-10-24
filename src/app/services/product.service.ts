9d6af2a7c9bb8342c9fc2789c84ca2f390c80569
aychakarma <karma.aycha@esprit.tn>
2023-10-24 23:25:11 +0100

    changes

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://localhost:8082'; 
+  private apiUrl = 'http://192.168.1.181:8083'; 
 
   constructor(private http: HttpClient) { }
 
